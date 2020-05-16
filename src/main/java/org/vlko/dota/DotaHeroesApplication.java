38d6f402e3aa6f82e4ddc56fa7d7af2582692f8a
vlko <petervlk86@gmail.com>
2020-05-16 20:23:01 +0200

    initial commit

---
+++ src/main/java/org/vlko/dota/DotaHeroesApplication.java
@@ -0,0 +1,13 @@
+package org.vlko.dota;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DotaHeroesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DotaHeroesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/vlko/dota/DotaHeroesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.vlko.dota;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DotaHeroesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
